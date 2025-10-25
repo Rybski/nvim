59e817b5f070eb51b0420d40d77c08a9fc973736
Rybski <michal.rylski98@gmail.com>
2025-10-26 01:27:50 +0200

    trouble
    - add: <leader>dd keybinding to display current file diagnostics
    - add: <leader>dp keybinding to display per project diagnostics
    - remove: <leader>d keybinding
    - fix: fixed panel not opening when no issues are detected
    - fix: fixed panel not auto focusing when opened

--- lua/plugins/trouble-nvim.lua
+++ lua/plugins/trouble-nvim.lua
@@ -3,9 +3,18 @@ return {
 	lazy = true,
 	cmd = "Trouble",
 	opts = {
+		warn_no_results = false,
+		open_no_results  = true,
+		height = 10,
+		modes = {
+    			diagnostics = {
+    				win = { position = "bottom", size = 10 }, -- consistent compact layout
+    			},
+		},
 	},
 	keys = {
-		{ "<leader>d", "<cmd>Trouble diagnostics toggle<cr>", desc="Open trouble" },
+		{ "<leader>dd", "<cmd>Trouble diagnostics toggle focus=True filter.buf=0<cr>", desc="Open local diagnostics" },
+		{ "<leader>dp", "<cmd>Trouble diagnostics toggle focus=True<cr>", desc="Open project diagnostics" },
 	},
 	dependencies = {
 		{ "echasnovski/mini.icons", opts={} },
